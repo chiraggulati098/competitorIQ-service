5cf288d4ae9315b03e9999badee06fc21858621f
Chirag Gulati <chiraggulati098@gmail.com>
2025-07-21 20:10:43 +0530

    minor fix

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def user_preferences():
         client.close()
         return jsonify({'success': True}), 200
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
